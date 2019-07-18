293a63e00a205138083f245bb50dee299e60092a
ry <2834016348@qq.com>
2019-07-18 17:28:45 +0800

    使用 webpack.config.js 和 npm scripts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import _ from 'lodash';
+/*import _ from 'lodash';
 import $ from 'jquery';
 
 function component() {
   const element = $('<div></div>');
 
-  element.html(_.join(['Hello!!', 'webpack'], ' '));
+  element.html(_.join(['Hello', 'webpack'], ' '));
 
   return element.get(0);
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component());*/
+
+console.log(122)
+
