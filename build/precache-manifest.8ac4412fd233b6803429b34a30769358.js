06f11dc7acfc48526c07a8d65251b19ad863e283
ry <2834016348@qq.com>
2019-08-02 16:54:40 +0800

    build

---
+++ build/precache-manifest.8ac4412fd233b6803429b34a30769358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9f22e07c5dd292fc0f3178055e3cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afcb71646da493fc2b2f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "afcb71646da493fc2b2f",
+    "url": "/static/js/main.2c83a72b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
