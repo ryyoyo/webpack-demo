9ac0207bf2d005feeef8d64524589f3e1f72efc9
ry <2834016348@qq.com>
2019-08-02 16:13:58 +0800

    使用 webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	devServer: {
+     	contentBase: './dist'
+    },
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+}
